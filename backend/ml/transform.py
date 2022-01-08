b2cb2179369409aa6dac2fd1f9ad22861b174982
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-01-08 12:59:49 -0800

    change transform.py to handle input img, rename stylegan2-ada-main to stylegan2-ada

--- backend/ml/transform.py
+++ backend/ml/transform.py
@@ -17,9 +17,9 @@ def transform(human_face):
     os.mkdir(TMP_DIR)
 
     human_face_path = TMP_DIR + HUMAN_IMG_NAME
-    cv2.imwrite(human_face_path, human_face)
+    human_face.save(human_face_path)
 
-    cat_num = random.rand(0, len([name for name in os.listdir(CAT_DIR) if os.path.isfile(os.path.join(DIR, name))]) - 1)
+    cat_num = random.rand(0, len([name for name in os.listdir(CAT_DIR) if os.path.isfile(os.path.join(CAT_DIR, name))]) - 1)
     cat_file = os.listdir(CAT_DIR)[cat_num]
 
     cat_face = cv2.cvtColor(CAT_DIR + cat_file, cv2.COLOR_BGR2RGB)
@@ -30,4 +30,4 @@ def transform(human_face):
 
     shutil.rmtree(TMP_DIR)
 
-    return True
+    return True, "/backend/output/movie.mp4"
