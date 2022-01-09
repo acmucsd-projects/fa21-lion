eaf46d8ed42fe0d363cfd5dd69790a7543c6743a
gaurav-dheeraj <64993922+gaurav-dheeraj@users.noreply.github.com>
2022-01-08 21:08:56 -0800

    Create register.py

---
+++ frontend/register.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+def app():
+  st.title("Create New Account")
+
+  new_user = st.text_input("Username",key='1')
+  new_password = st.text_input("Password",type='password',key='2')
+
+  if st.button("Register"):
+    if len(new_user) < 5:
+      st.error("Enter a username that is at least 5 characters.")
+    if len(new_password) < 8:
+      st.error("Enter a password that is at least 8 characters.")
+    else:
+      # TODO (Aman): Add your logic for registering under here
+      # result = add_userdata(new_user,new_password)
+      # I have assumed result returns false if username already exists
+      # and true if registered successfully. Feel free to add more cases
+      # if needed (eg. username is unique but failed to save in DB)
+      # REMOVE NEXT LINE
+
+      result = True
+
+      if result:
+        st.success("You have successfully created an Account")
+        st.info("Use the Left Sidebar to create a new cat video view your existing videos")
+      else:
+        st.error("Username already exists.")
