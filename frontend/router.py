1191309063940e8168c5da552fbdc38ee893899e
gaurav-dheeraj <64993922+gaurav-dheeraj@users.noreply.github.com>
2022-01-08 21:32:31 -0800

    Update router.py

--- frontend/router.py
+++ frontend/router.py
@@ -4,6 +4,7 @@ import streamlit as st
 from multipage import MultiPage
 import landing
 import register
+import allvideos
 
 # Create an instance of the app 
 app = MultiPage()
@@ -13,7 +14,8 @@ st.set_page_config(page_title="Team Lion", layout="wide")
 
 # Add all your applications (pages) here
 app.add_page("Get Your Cat Video", landing.app)
-app.add_page("Create a New Account", register.app)
+app.add_page("Create New Account", register.app)
+app.add_page("View Saved Videos", allvideos.app)
 
 # The main app
 app.run()
