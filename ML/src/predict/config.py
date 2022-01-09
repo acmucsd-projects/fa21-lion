18c3f8b7ac30d95ae361700876c9596b9fe6fb9e
Arth Shukla <44527268+arth-shukla@users.noreply.github.com>
2022-01-08 18:44:06 -0800

    update config and predict

--- ML/src/predict/config.py
+++ ML/src/predict/config.py
@@ -1,13 +1,26 @@
 import torch
 
-NETWORK = "./network-snapshot.pkl"
-STEPS = 600
-SKIPPED_FRAME_CAP = 10
+#all
+TMP_DIR = "./tmp/"
+
+# transform script
+CAT_DIR = "./cat_images/"
+HUMAN_IMG_NAME = "human_face.png"
+PREDICT_PATH = "./predict.py"
+
+#predict script
+NETWORK = "./weights/network-snapshot.pkl"
+STEPS = 2000
+SKIPPED_FRAME_CAP = 20
 FPS = 30
 FREEZE_STEPS = 30
 NUM_STEPS = 1000
 SAVE_VIDEO = 0
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-STORAGE_DIR = "./aligned_images/"
-PROJECTED_DIR = {"source": "./projected/source/", "target": "./projected/target/"}
+STORAGE_DIR = TMP_DIR + "aligned_images/"
+PROJECTED_DIR = {
+    "projected": TMP_DIR + "projected/",
+    "source": TMP_DIR + "projected/source/", 
+    "target": TMP_DIR + "projected/target/"
+}
 MP4_DIR = "./output/"
